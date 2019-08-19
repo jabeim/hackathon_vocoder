0a0806137a413661b518fcc2de67d7248bacbea1
jabeim <49572428+jabeim@users.noreply.github.com>
2019-08-19 15:49:45 -0500

    disabled ab standard vocoder by default
    functionality can be restored by uncommenting lines related to standard voc (ln 14, 32, 43)

--- vocoder_demo.py
+++ vocoder_demo.py
@@ -11,7 +11,7 @@ import numpy as np
 import pyaudio as pa
 from scipy.signal import resample
 from scipy.io.wavfile import read
-from ab_standard import vocoder as standard_voc
+#from ab_standard import vocoder as standard_voc
 from ab_spiral import vocoder as hybrid_voc
 #import cProfile
 
@@ -29,7 +29,7 @@ for fNum in nums:
     #%% Process and visualize data
     
     
-    standardOut,outFs = standard_voc(fn,resistorVal=10,nl=nlVal,MCLmuA = 542)
+#    standardOut,outFs = standard_voc(fn,resistorVal=10,nl=nlVal,MCLmuA = 542)
     hybridOut,outFs = hybrid_voc(fn,resistorVal=10,nl=nlVal,MCLmuA = 542)
     wavIn = read(fileLoc+wavN+'.wav')
     wavData = wavIn[1]/(2**15-1)
@@ -40,7 +40,7 @@ for fNum in nums:
     #%% Play Audio Output
     if playAudio:
         input1 = np.float32(np.concatenate((wavResampled,np.zeros(Fs))))
-        output1 = np.float32(standardOut)
+#        output1 = np.float32(standardOut)
         # add zeros before second output sample
         output2 = np.float32(np.concatenate((np.zeros(Fs),hybridOut)))
         p = pa.PyAudio()
@@ -54,10 +54,10 @@ for fNum in nums:
                         output_device_index = devIndex
                         )
         inData = input1.astype(np.float32).tostring()
-        outData1 = output1.astype(np.float32).tostring()
+#        outData1 = output1.astype(np.float32).tostring()
         outData2 = output2.astype(np.float32).tostring()
         
         stream.write(inData)
-        stream.write(outData1)
+#        stream.write(outData1)
         stream.write(outData2)
         stream.close()
